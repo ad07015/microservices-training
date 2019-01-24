c273a86ce7e24c3a2fb7e1dfe3e076e80f65a484
andrejs.dasko <andrejs.dasko@accenture.com>
2019-01-24 23:08:06 +0200

    Generic discounts are now returned if no UUID is provided to the /offers endpoint. Loyalty service is not called in this case.

--- discount/src/main/resources/import.sql
+++ discount/src/main/resources/import.sql
@@ -1,4 +1,4 @@
-insert into DISCOUNT (id, discount, points, play) values(20001,10,100,'10% off your ticket');
-insert into DISCOUNT (id, discount, points, play) values(20002,20,400,'20% off your ticket');
-insert into DISCOUNT (id, discount, points, play) values(20003,30,900,'30% off your ticket');
-insert into DISCOUNT (id, discount, points, play) values(20004,40,1600,'40% off your ticket');
+insert into DISCOUNT (id, discount, points, play) values(20001,10,100,'Hamlet');
+insert into DISCOUNT (id, discount, points, play) values(20002,20,400,'Macbeth');
+insert into DISCOUNT (id, discount, points, play) values(20003,30,900,'The Phantom of the Opera');
+insert into DISCOUNT (id, discount, points, play) values(20004,40,1600,'My Fair Lady');
--- offer/src/main/java/com/training/microservices/offer/resource/OfferResource.java
+++ offer/src/main/java/com/training/microservices/offer/resource/OfferResource.java
@@ -9,7 +9,9 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.websocket.server.PathParam;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -20,15 +22,22 @@ public class OfferResource {
     public static final int MAX_DISCOUNT_COUNT = 10;
 
     @GetMapping("/offers")
-    public DiscountList getOffers(@PathParam("uuid") String uuid) {
+    public DiscountList getOffers(@PathParam("uuid") Optional<String> uuid) {
         DiscountList discountList = new RestTemplate().getForObject(DISCOUNTS_URL, DiscountList.class);
-        LoyaltyPoints loyaltyPoints = getLoyaltyPoints(uuid);
+        if (uuid.isPresent()) {
+            LoyaltyPoints loyaltyPoints = getLoyaltyPoints(uuid.get());
+            List<Discount> affordableDiscounts = discountList.getDiscountList().stream()
+                    .filter(disc -> disc.getPoints() <= loyaltyPoints.getPoints())
+                    .limit(MAX_DISCOUNT_COUNT)
+                    .collect(Collectors.toList());
+            return new DiscountList(affordableDiscounts);
+        } else {
+            LinkedList<Discount> genericDiscounts = new LinkedList<>();
+            genericDiscounts.add(new Discount(20001L, 5L, 0L, "5% off any play"));
+            genericDiscounts.add(new Discount(20002L, 10L, 0L, "10% off any play when buying two tickets"));
+            return new DiscountList(genericDiscounts);
+        }
 
-        List<Discount> affordableDiscounts = discountList.getDiscountList().stream()
-                .filter(disc -> disc.getPoints() <= loyaltyPoints.getPoints())
-                .limit(MAX_DISCOUNT_COUNT)
-                .collect(Collectors.toList());
-        return new DiscountList(affordableDiscounts);
     }
 
     private LoyaltyPoints getLoyaltyPoints(String uuid) {
